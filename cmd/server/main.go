043e60ee6f10c78b84c3511abdcd5bbf71fd4f55
Omar Jarbou <omarjarbou2019@gmail.com>
2025-10-22 13:07:22 +0300

    Implement REPL command handling for pause, resume, quit, and help in server main function; integrate gamelogic for user input processing.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -30,13 +31,41 @@ func main() {
 		return
 	}
 
-	pause_msg := routing.PlayingState{
-		IsPaused: true,
-	}
-	err = pubsub.PublishJSON(chann, routing.ExchangePerilDirect, routing.PauseKey, pause_msg)
-	if err != nil {
-		log.Fatal("Error while publishing pause message: " + err.Error())
-		return
+	gamelogic.PrintServerHelp()
+	for {
+		words := gamelogic.GetInput()
+		if len(words) != 1 {
+			fmt.Println("you must enter a one word command from possible commands list.")
+			continue
+		}
+		if words[0] == "pause" {
+			fmt.Println("Publishing pause message...")
+			pause_msg := routing.PlayingState{
+				IsPaused: true,
+			}
+			err = pubsub.PublishJSON(chann, routing.ExchangePerilDirect, routing.PauseKey, pause_msg)
+			if err != nil {
+				log.Fatal("Error while publishing pause message: " + err.Error())
+				return
+			}
+		} else if words[0] == "resume" {
+			fmt.Println("Publishing resume message...")
+			pause_msg := routing.PlayingState{
+				IsPaused: false,
+			}
+			err = pubsub.PublishJSON(chann, routing.ExchangePerilDirect, routing.PauseKey, pause_msg)
+			if err != nil {
+				log.Fatal("Error while publishing resume message: " + err.Error())
+				return
+			}
+		} else if words[0] == "exit" {
+			fmt.Println("Exiting...")
+			break
+		} else if words[0] == "help" {
+			gamelogic.PrintServerHelp()
+		} else {
+			fmt.Println("Unknown Command")
+		}
 	}
 
 	// wait for ctrl+c
